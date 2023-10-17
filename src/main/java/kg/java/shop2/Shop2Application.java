5c3c5f51d8120d61b70cb583e4982ed10ba2248b
User <stparchkg@gmail.com>
2023-10-17 21:19:12 +0600

    setup empty spring project

---
+++ src/main/java/kg/java/shop2/Shop2Application.java
@@ -0,0 +1,13 @@
+package kg.java.shop2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shop2Application.class, args);
+	}
+
+}
